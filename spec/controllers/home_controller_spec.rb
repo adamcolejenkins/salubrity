e0852204c0ad41fd2fe5f67652450d1a46c5e4b2
Adam Jenkins <adamcole83@gmail.com>
2014-09-18 21:03:35 -0500

    Initial commit

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, :type => :controller do
+  let(:user) { create(:user) }
+
+  describe "#index" do
+    it "should render when user is not signed in" do
+      get :index
+      response.should be_ok
+      response.should render_template('index')
+    end
+
+    it "should redirect to the dashboard if signed in" do
+      sign_in(user)
+      get :index
+      response.should redirect_to(dashboard_path)
+    end
+  end
+
+end
