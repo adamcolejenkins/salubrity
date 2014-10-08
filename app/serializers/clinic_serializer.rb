25dc54e2d0115c6af600e1453078be18e891ecd6
Adam Jenkins <adamcole83@gmail.com>
2014-10-08 11:06:23 -0500

    Removed serializers, using jBuilder

--- app/serializers/clinic_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ClinicSerializer < ActiveModel::Serializer
-  attributes :id, :title, :address, :address2, :city, :state, :zip, :phone, :guid, :providers
-  has_one :survey
-  has_many :providers
-end
--- app/serializers/provider_serializer.rb
+++
@@ -1,3 +0,0 @@
-class ProviderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :position, :email, :phone, :clinic, :photo, :created_at, :updated_at
-end
