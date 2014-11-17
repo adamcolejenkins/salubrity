b2a2eb10ff7deacdd6fde5af1cd6635343dc7695
Adam Jenkins <adamcole83@gmail.com>
2014-11-17 12:52:46 -0600

    Testing charts

--- app/controllers/dashboard/survey_controller.rb
+++ app/controllers/dashboard/survey_controller.rb
@@ -20,6 +20,16 @@ class Dashboard::SurveyController < ApplicationController
     render json: @survey.responses.daily_avg_time
   end
 
+  def multiple_choice_chart
+    @field = @survey.fields.find(params[:field_id])
+    render json: @survey.data_for(:multiple_choice_field, field: @field)
+  end
+
+  def multiple_choice_color_chart
+    @field = @survey.fields.find(params[:field_id])
+    render json: @survey.data_for(:multiple_choice_field_colors, field: @field)
+  end
+
   def set_survey
     @survey = current_team.surveys.find(params[:id])
   end
