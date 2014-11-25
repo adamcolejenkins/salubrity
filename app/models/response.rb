f549249ca7662e9dc7db74172fc863a95e8369cd
Adam Jenkins <adamcole83@gmail.com>
2014-11-25 13:34:14 -0600

    Fixed issue where some answers weren't being stored if blank

--- app/models/response.rb
+++ app/models/response.rb
@@ -6,7 +6,7 @@ class Response < ActiveRecord::Base
   belongs_to :clinic, inverse_of: :responses
   belongs_to :provider, inverse_of: :responses
   has_many :answers, -> { includes :field }, autosave: true
-  accepts_nested_attributes_for :answers, reject_if: proc { |attributes| attributes['value'].blank? }
+  accepts_nested_attributes_for :answers, reject_if: proc { |attributes| attributes['value'].nil? }
 
   def time
     (self.ended_at.to_f - self.started_at.to_f).to_f
