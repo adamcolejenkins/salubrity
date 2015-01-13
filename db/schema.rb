14cec133072bafe96cb18c65b2021bd8b4f0c487
Adam Jenkins <adamcole83@gmail.com>
2015-01-13 10:42:55 -0600

    Rails and component maintenance.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210192913) do
+ActiveRecord::Schema.define(version: 20150105195220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "answers", force: true do |t|
     t.integer  "response_id"
